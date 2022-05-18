574b94d457260bdfab739e608ba42e3474fdfd64
seda615 <grigoryanseda96@gmail.com>
2022-05-18 10:05:13 +0400

    folder name change

---
+++ src/container/List/index.js
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+
+class List extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {list: []}
+    }
+
+    addHighestAverage = () => {
+
+        const {data} = this.props;
+
+        const {maxAverageIndex, maxAverage} = this.props.addHighestAverage();
+        if (maxAverageIndex !== undefined) {
+            this.setState({list: [...this.state.list, {...data[maxAverageIndex], rateAverage: maxAverage}]})
+        }
+    }
+
+    sort = () => {
+        const {list} = this.state;
+
+        this.setState(list.sort((l1, l2) => l1.rateAverage - l2.rateAverage ))
+
+    }
+
+    removeColumn = (index, id) => {
+        const {list} = this.state;
+
+        this.setState(list.splice(index, 1));
+        this.props.removeColumn(id);
+    }
+
+    render() {
+
+        const {list} = this.state;
+
+        return (
+            <div className="list">
+                <button onClick={this.addHighestAverage}>+</button>
+                <button onClick={this.sort}>sort</button>
+                <ul>
+                    {list.map((list, index) => (
+                        <li key={list.id}>
+                            <p>{list.name}</p><p> {list.rateAverage}</p>
+                            <button onClick={() => this.removeColumn(index, list.id)}>-</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default List
